c3a645dadf09c80f01097439575f9183e06d12fd
DaniilTararykov <73693946+DaniilTararykov@users.noreply.github.com>
2020-11-02 10:59:47 +0100

    Update R_first_Code.r

--- R_first_Code.r
+++ R_first_Code.r
@@ -1,3 +1,31 @@
-1. # Hi
-2.
-3. 3, 
+# Hi
+
+
+primates <- c(3, 5, 9, 15, 40)
+primates
+
+# new set:
+crabs <- c(100, 70, 30, 10, 5)
+crabs
+
+# first plot in R
+plot(primates, crabs)
+
+plot (primates, crabs, col="red", pch=19)
+
+
+plot (primates, crabs, col="red", pch=19, cex=2)
+
+plot (primates, crabs, col="red", pch=19, cex=6)
+
+plot (primates, crabs, col="red", pch=19, cex=6, main="My first ecological graph in R!")
+
+# create a dataframe
+ecoset <- data.frame(primates, crabs)
+
+# ecological numbers
+# mean number of individuals per site
+(3+5+9+15+40)/5
+
+# summary of the dataframe
+summary(ecoset)
