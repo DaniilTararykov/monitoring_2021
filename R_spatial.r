32d11c2897228f680f2b1c831b04b323f4d4b90e
DaniilTararykov <73693946+DaniilTararykov@users.noreply.github.com>
2020-11-13 10:49:11 +0100

    Create R_spatial.r

---
+++ R_spatial.r
@@ -0,0 +1,26 @@
+# R spatial
+
+install.packages("sp")
+library(sp)
+
+data(meuse)
+
+head(meuse)
+
+coordinates(meuse) = ~x+y
+plot(meuse)
+
+# splot is used to plot elements like zinc, lead etc.
+spplot(meuse, "zinc", main="Concentration of zinc")
+
+# excercise 1
+spplot(meuse, "copper", main="Concentration of copper")
+
+# excercise 2 
+spplot(meuse, c("copper", "zinc"))
+
+bubble(meuse, "zinc")
+
+# excercise 3
+bubble(meuse, "lead")
+bubble(meuse, "lead", col="blue")
