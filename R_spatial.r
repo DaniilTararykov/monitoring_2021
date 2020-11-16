e44fba6447c6aadf5eba8fd6f50a603befbde933
DaniilTararykov <73693946+DaniilTararykov@users.noreply.github.com>
2020-11-16 10:58:11 +0100

    Update R_spatial.r

--- R_spatial.r
+++ R_spatial.r
@@ -28,4 +28,24 @@ bubble(meuse, "lead", col="blue")
 
 
 # installing the ggplot2 library 
+install.packages("ggplot2")
+library(ggplot2)
+# ecological data frame 
+# biofuels
+biofuels c<- 120, 200, 350, 570, 750 #array of values: c
+# oxydative enzimes 
+oxydative <- c(1200, 1300, 21000, 34000, 50000)
+d <- data.frame(biofuels, oxydative)
+d
+ggplot(d, aes(x=biofuels, y=oxydative)) + geom_point()
 
+ggplot(d, aes(x=biofuels, y=oxydative)) + geom_point(size=5, color="red")
+ggplot(d, aes(x=biofuels, y=oxydative)) + geom_point(size=5, color="red")+ geom_line()
+ggplot(d, aes(x=biofuels, y=oxydative)) + geom_polygon()
+
+# import data
+# setwd ("path/lab")
+setwd ("/Users/hausofdanik/lab/")
+
+
+setwd("/Users/hausofdanik/Documents/lab")
