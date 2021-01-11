20f918676105c462c0f22262446189101f4cdf84
DaniilTararykov <73693946+DaniilTararykov@users.noreply.github.com>
2021-01-11 09:26:37 +0100

    Update R_code_remote_sensing.r

--- R_code_remote_sensing.r
+++ R_code_remote_sensing.r
@@ -1,1 +1,22 @@
 install.packages(c("raster", "RStoolbox")) 
+#R code for remote sensing data analysis to monitor ecosystem chianging in space and time
+install.packages(c("raster", "RStoolbox"))
+library(raster)
+library(RStoolbox)
+#set the work directori
+setwd("/Users/hausofdanik/Documents/lab")
+#upload images and give file a name
+p224r63_2011<-brick("p224r63_2011_masked.grd")
+#check metadata
+p224r63_2011
+#plot graph to see single band
+plot(p224r63_2011)
+#put different color palette for every band
+cl <- colorRampPalette(c('black','grey','light grey'))(100) # 
+plot(p224r63_2011, col=cl)
+#plot only first 4 bands in 2 rows and 2 columns
+par(mfrow=c(2,2))
+
+
+clb <- colorRampPalette(c('dark blue','blue','light blue'))(100) # 
+plot(p224r63_2011$B1_sre, col=clb)
